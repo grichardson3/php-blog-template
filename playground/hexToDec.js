40fa8f8787fdabd97348172b8b7d15defb6f17a5
Gareth Richardson <grichardson@live.ca>
2020-07-22 16:57:04 -0400

    removed gitignore

---
+++ playground/hexToDec.js
@@ -0,0 +1,17 @@
+// Hex Converter
+const hexVal = "ffffff"; // Max hex value
+
+function hexToDec() {
+  return parseInt(hexVal, 16);
+}
+
+console.log(hexToDec());
+
+// Binary Converter
+/*const binaryVal = "00000010";
+
+function binaryToDec() {
+  return parseInt(binaryVal, 2);
+}
+
+console.log(binaryToDec());*/
